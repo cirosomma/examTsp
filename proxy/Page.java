72b518ef44790e35235d0d3cb9c4599cb6ed6693
ciro somma <miclen.developers@gmail.com>
2015-10-07 18:59:22 +0200

    Proxy Pattern

---
+++ proxy/Page.java
@@ -0,0 +1,11 @@
+/*
+    Fornisce un surrogato di un altro oggetto per controllare l'accesso a tale
+    oggetto. In pratica creiamo un oggetto avente la stessa interfaccia
+    dell'oggetto che si vuole controllare.
+    Obiettivi:
+    - Controllare accessi per differenti client
+    - Non tutti i client dovrebbero essere autorizzati a usare i servizi di un
+      componente
+    - Utilizzabile in ogni ambiente distribuito per 
+
+*/
